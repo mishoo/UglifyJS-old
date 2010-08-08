5a89d13ecd3f26a79125b25df627e1f8996aa05e
Mihai Bazon <mihai@bazon.net>
2010-08-08 20:05:17 +0200

    disorganized tests

---
+++ test/beautify.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+global.sys = require("sys");
+var fs = require("fs");
+
+var jsp = require("../lib/parse-js");
+
+var filename = process.argv[2];
+fs.readFile(filename, "utf8", function(err, text){
+        try {
+                var ast = time_it("parse", function(){ return jsp.parse(text); });
+                //sys.puts(JSON.stringify(ast));
+                var ast2 = time_it("process", function(){ return jsp.process_ast(ast) });
+                var gen = time_it("generate", function(){ return jsp.gen_code(ast2, true) });
+                sys.puts(gen);
+        } catch(ex) {
+                sys.puts(ex.stack);
+                sys.puts(sys.inspect(ex));
+        }
+});
+
+function time_it(name, cont) {
+        var t1 = new Date().getTime();
+        var ret = cont();
+        var diff = new Date().getTime() - t1;
+        sys.puts("// " + name + ": " + (diff / 1000).toFixed(3) + " sec.");
+        return ret;
+};
