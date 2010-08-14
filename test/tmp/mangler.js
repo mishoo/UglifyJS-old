c9a7efa5a8ce0a7f1c76396d9a74a77fb147d966
Mihai Bazon <mihai@bazon.net>
2010-08-14 14:05:19 +0200

    another fix: don't shadow a name that is referenced but not defined -- it's possibly a global defined elsewhere

--- test/tmp/mangler.js
+++ test/tmp/mangler.js
@@ -9,25 +9,39 @@
         };
 })();
 
-
-(function(B){
+(function(b){
         // B remains unmangled because eval is used in sub-scope
         (function(){ return eval("parc") })(); // so we have eval
         (function(){
                 // here we are safe from eval() so this scope is mangled
                 var foo = 5;
-                // but bar should NOT mangle to B, because we reference it!
+                // but bar should NOT mangle to b, because we reference it!
                 var bar = 10;
-                return B + foo + bar;
+                return b + foo + bar;
         })();
 })();
 
 (function(){
-        var mak = 10;
-        return (function(){
-                var q;
+        var mak = 10;           // mangles to 'a'
+        out(function(){
+                var q;          // should NOT mangle to 'a', because
+                                // it would shadow a name that is in
+                                // use below:
                 return (function(){
                         return mak;
                 })();
+        });
+        out(function(){
+                var q = 5;      // can (and does) mangle to 'a'
+                return q;
+        });
+})();
+
+// reference a() -- a name possibly defined elsewhere.  Make sure we
+// don't shadow it with a mangled name.
+(function(){
+        var mak = 10;           // should NOT mangle to 'a'
+        return (function(){
+                return a(mak);
         })();
 })();
