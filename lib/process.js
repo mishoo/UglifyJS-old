mishoo/UglifyJS-old

commit ae88177fde6a7e54b047ee0d387ee539126016c2

mangled name of a function expression should not clash with a name in the parent scope, in order to deal with a 15 years-old IE bug. (fixes #161)
thanks everyone for the discussion and patience!

lib/process.js
@@ -339,16 +339,17 @@ Scope.prototype = {
                         return m;
                 }
         },
+        set_mangle: function(name, m) {
+                this.rev_mangled[m] = name;
+                return this.mangled[name] = m;
+        },
         get_mangled: function(name, newMangle) {
                 if (this.uses_eval || this.uses_with) return name; // no mangle if eval or with is in use
                 var s = this.has(name);
                 if (!s) return name; // not in visible scope, no mangle
                 if (HOP(s.mangled, name)) return s.mangled[name]; // already mangled in this scope
                 if (!newMangle) return name;                      // not found and no mangling requested
-
-                var m = s.next_mangled();
-                s.rev_mangled[m] = name;
-                return s.mangled[name] = m;
+                return s.set_mangle(name, s.next_mangled());
         },
         define: function(name) {
                 if (name != null)
@@ -484,7 +485,9 @@ function ast_mangle(ast, options) {
                 var is_defun = this[0] == "defun";
                 if (is_defun && name) name = get_mangled(name);
                 body = with_scope(body.scope, function(){
-                        if (!is_defun && name) name = get_mangled(name);
+                        if (!is_defun && name) {
+                                name = scope.set_mangle(name, scope.parent.next_mangled());
+                        }
                         args = MAP(args, function(name){ return get_mangled(name) });
                         return MAP(body, walk);
                 });

commit 6a873a7757895fdc06501854e4ba84e15c12a74a

fixed regression after ae88177fde6a7e54b047: mangling the name of function expressions might clash with an identifier that's already in scope if eval/when is in use.
fixes #170

lib/process.js
@@ -487,7 +487,10 @@ function ast_mangle(ast, options) {
                         if (is_defun) name = get_mangled(name);
                         else {
                                 extra = {};
-                                name = extra[name] = scope.next_mangled();
+                                if (!(scope.uses_eval || scope.uses_with))
+                                        name = extra[name] = scope.next_mangled();
+                                else
+                                        extra[name] = name;
                         }
                 }
                 body = with_scope(body.scope, function(){