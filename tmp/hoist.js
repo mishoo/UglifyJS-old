5221a3585d0896efc8bab83829462bfbe0700e16
Mihai Bazon <mihai@bazon.net>
2011-07-29 23:15:59 +0300

    Added ast_lift_variables, which does a little more than its name says:
    - move all var declarations to the top of the scope
    - drops redundant var declarations
    - removes unused function arguments
    - removes unreferenced inner functions
    
    It's commented out for now in bin/uglifyjs.  It seems safe, but however the
    generated code is a bit longer.  That's mainly because:
    
        var a = 1, b = 2, c = 3;
    
    becomes:
    
        var a, b, c;
        a = 1, b = 2, c = 3;
    
    I will improve on this.

---
+++ tmp/hoist.js
@@ -0,0 +1,30 @@
+function foo(arg1, arg2, arg3, arg4, arg5, arg6) {
+        var a = 5, arg4 = 1;
+        var d = 10, mak = 20, buz = 30;
+        if (moo) {
+                var a, b, c;
+        }
+        for (var arg1 = 0, d = 20; arg1 < 10; ++arg1)
+                console.log(arg3);
+        for (var i in mak) {}
+        for (j in buz) {}
+        var d;
+
+        function test() {
+                
+        };
+
+        //test();
+
+        (function moo(first, second){
+                console.log(first);
+        })(1);
+
+        (function moo(first, second){
+                console.log(moo());
+        })(1);
+}
+
+
+var foo;
+var bar;
