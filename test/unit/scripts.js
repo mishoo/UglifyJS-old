0c67f5723faeac64eb6e30d78b4811e29251e984
Mihai Bazon <mihai@bazon.net>
2011-08-15 14:49:38 +0300

    moved Array ==> [] transforms in squeeze-more (so they now apply on `--unsafe`)

--- test/unit/scripts.js
+++ test/unit/scripts.js
@@ -12,7 +12,7 @@ var scriptsPath = __dirname;
 function compress(code) {
 	var ast = jsp.parse(code);
 	ast = pro.ast_mangle(ast);
-	ast = pro.ast_squeeze(ast, {no_warnings: true, extra: true});
+	ast = pro.ast_squeeze(ast, { no_warnings: true });
         ast = pro.ast_squeeze_more(ast);
 	return pro.gen_code(ast);
 };
